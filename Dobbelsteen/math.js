b899a536c62017a5002628b7ca36542ac7d45050
walid-elaazmani <walid.elaazmani@gmail.com>
2023-05-25 11:23:55 +0200

    update
    added dice + dice changing in function of random number

--- Dobbelsteen/math.js
+++ Dobbelsteen/math.js
@@ -6,27 +6,36 @@ function dobbelsteenGooien(){
 
     var aa = document.getElementById("gooi");
 
+    var d1 = document.getElementById("1");
+    var d2 = document.getElementById("2");
+    var d3 = document.getElementById("3");
+    var d4 = document.getElementById("4");
+    var d5 = document.getElementById("5");
+
+
     var myArray = [0,0,0,0,0,0,0];
+    var imgArray = [d1,d2,d3,d4,d5];
 
 
     for(let i = 0; i<5; i++ ){
         var a = Math.floor(Math.random() * 6) + 1;
        switch(a){
-        case 1 : myArray[0] ++;
+        case 1 : myArray[0] ++; imgArray[i].src=".\\Resources\\dobbel1.gif";
         break;
-        case 2 : myArray[1] ++;
+        case 2 : myArray[1] ++; imgArray[i].src=".\\Resources\\dobbel2.gif";
         break;
-        case 3 : myArray[2] ++;
+        case 3 : myArray[2] ++; imgArray[i].src=".\\Resources\\dobbel3.gif";
         break;
-        case 4 : myArray[3] ++;
+        case 4 : myArray[3] ++; imgArray[i].src=".\\Resources\\dobbel4.gif";
         break;
-        case 5 : myArray[4] ++;
+        case 5 : myArray[4] ++; imgArray[i].src=".\\Resources\\dobbel5.gif";
         break;
-        case 6 : myArray[5] ++;
-       }
+        case 6 : myArray[5] ++; imgArray[i].src=".\\Resources\\dobbel6.gif";
     }
+}
 
 
+    
 
     if(Math.max(...myArray) > myMaxScore){
         myMaxScore = Math.max(...myArray);
